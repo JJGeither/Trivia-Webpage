d357639b16f225cae4802bcfbba6b1687c3e152a
JJGeither <77400399+JJGeither@users.noreply.github.com>
2023-10-14 11:47:03 -0400

    Added Basic Webpage

--- trivia-webpage/src/App.jsx
+++ trivia-webpage/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [question, setQuestion] = useState('');
+    const [options, setOptions] = useState([]);
+    const [selectedOption, setSelectedOption] = useState('');
+    const [correctAnswer, setCorrectAnswer] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleOptionSelect = (option) => {
+        setSelectedOption(option);
+    };
+
+    const handleAnswerSubmit = () => {
+        if (selectedOption === correctAnswer) {
+            alert('Correct!');
+        } else {
+            alert('Incorrect!');
+        }
+    };
+
+    const loadTrivia = () => {
+        setQuestion('What is the capital of France?');
+        setOptions(['London', 'Berlin', 'Madrid', 'Paris']);
+        setCorrectAnswer('Paris');
+    };
+
+    return (
+        <div className="trivia-app">
+            
+            <nav className="navbar">
+                <h1 className="navbar-title">Trivia Game</h1>
+            </nav>
+            <div className="trivia-card">
+                <p className="trivia-question">{question}</p>
+                <div className="trivia-options">
+                    {options.map((option, index) => (
+                        <div
+                            key={index}
+                            className={`trivia-option ${selectedOption === option ? 'selected' : ''}`}
+                            onClick={() => handleOptionSelect(option)}
+                        >
+                            {option}
+                        </div>
+                    ))}
+                </div>
+                <button
+                    className="trivia-submit-button"
+                    onClick={handleAnswerSubmit}
+                    disabled={!selectedOption}
+                >
+                    Submit Answer
+                </button>
+            </div>
+            <button className="load-trivia-button" onClick={loadTrivia}>
+                Load New Trivia
+            </button>
+        </div>
+    );
 }
 
-export default App
+export default App;
