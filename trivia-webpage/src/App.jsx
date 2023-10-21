e92fd07b325d2ae73f6daba734e4c2399e951c05
JJGeither <77400399+JJGeither@users.noreply.github.com>
2023-10-21 17:46:57 -0400

    Update App.jsx

--- trivia-webpage/src/App.jsx
+++ trivia-webpage/src/App.jsx
@@ -28,28 +28,21 @@ function App() {
         setCorrectAnswer('Paris');
     };
 
+
     return (
 
         <div className="trivia-app">
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative', top: 0, left: '50%', width: '300px', height: '150px', marginLeft: '-151px', backgroundColor: "#283249", zIndex: 999, borderRadius: '10px', backgroundImage: 'linear-gradient(to right, #ff5d6c 50%, #35cdce 50%)' }}>
-                <div style={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: '0.5rem', backgroundColor: '#000', opacity: 0.3, borderRadius: '0 0 10px 10px' }}></div>
 
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'absolute', top: '1vw', left: '2vw', width: '200px', height: '100px', backgroundColor: "#283249", zIndex: 999, borderRadius: '10px', }}>
-                    <div style={{ padding: "1rem" }}>
-                        <img src={logo} alt="logo" className="h-300 w-300" />
-                    </div>
-                </div>
 
-            </div>
 
-            <div className="navbar bg-base-1000 flex items-center justify-center" style={{ height: '5rem', marginTop: '-150px', position: 'relative', overflow: 'hidden', backgroundImage: 'linear-gradient(to right, #ff5d6c 50%, #35cdce 50%)' }}>
+
+            <div className="navbar bg-base-1000 flex items-center justify-center" style={{ height: '5rem', marginTop: '0px', position: 'relative', overflow: 'hidden',  backgroundImage: 'linear-gradient(to right, #ff5d6c 47%, #35cdce 53%)' }}>
                 <div style={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: '0.5rem', backgroundColor: '#000', opacity: 0.3 }}></div>
-
 
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
                             </svg>
                         </label>
@@ -63,13 +56,13 @@ function App() {
 
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
                     </button>
                     <button className="btn btn-ghost btn-circle">
                         <div className="indicator">
-                            <svg xmlns="http://w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v 3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                             </svg>
                             <span className="badge badge-xs badge-primary indicator-item"></span>
@@ -78,11 +71,30 @@ function App() {
                 </div>
             </div>
 
-            <div className="flex justify-center items-center" style={{ marginTop: '500px'}}>
-                <button style={{ backgroundColor: '#F8C85F', color: '#283249' }} className="px-24 py-6 rounded-full hover:bg-yellow-600">
-                    <span className="font-bold text-xl">Centered Button</span>
+            <div className="flex flex-col items-center h-screen">
+                <div className="flex space-x-4">
+                    <div className="w-20 h-20 bg-[#ff5d6c] flex items-center justify-center rounded-md text-white font-retro text-4xl">
+                        Q
+                    </div>
+                    <div className="w-20 h-20 bg-[#35cdce] flex items-center justify-center rounded-md text-white font-retro text-4xl">
+                        U
+                    </div>
+                    <div className="w-20 h-20 bg-[#ff5d6c] flex items-center justify-center rounded-md text-white font-retro text-4xl">
+                        I
+                    </div>
+                    <div className="w-20 h-20 bg-[#35cdce] flex items-center justify-center rounded-md text-white font-retro text-4xl">
+                        Z
+                    </div>
+                </div>
+                <button className="btn btn-lg w-96 mt-4" style={{ backgroundColor: '#F8C85F', color: '#283249', fontSize: '24px' }}>
+                    <span className="font-bold text-4xl">Start quiz!</span>
                 </button>
             </div>
+
+
+
+
+
 
             <div className="trivia-card">
                 <p className="trivia-question">{question}</p>
